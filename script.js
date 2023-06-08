c13d1c8a04cba401a3d1cfed210645c13100fb1a
anasshahid315 <135336425+anasshahid315@users.noreply.github.com>
2023-06-08 11:34:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+const options = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "eceb355578msh18bdbfd1d233949p1b29e0jsn9e68cb68f7a0",
+    "X-RapidAPI-Host": "weather-by-api-ninjas.p.rapidapi.com",
+  },
+};
+
+const getWeather = (city) =>{
+	  cityName.innerHTML = city
+
+    fetch("https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=" + city, options)
+    .then((response) => response.json())
+    .then((response) => {
+        console.log(response);
+
+        cloud_pct.innerHTML = response.cloud_pct;
+        temp.innerHTML = response.temp;
+        temp2.innerHTML = response.temp;
+        feels_like.innerHTML = response.feels_like;
+        humidity.innerHTML = response.humidity;
+        humidity2.innerHTML = response.humidity;
+        min_temp.innerHTML = response.min_temp;
+        max_temp.innerHTML = response.max_temp;
+        wind_speed.innerHTML = response.wind_speed;
+        wind_speed2.innerHTML = response.wind_speed;
+        wind_degrees.innerHTML = response.wind_degrees;
+        sunrise.innerHTML = response.sunrise;
+        sunset.innerHTML = response.sunset;
+
+
+
+    })
+  .catch((err) => console.error(err));
+
+}
+
+submit.addEventListener("click", (event)=>{
+	event.preventDefault()
+	getWeather(city.value)
+})
+
+
+getWeather("Delhi")
+
+
+
+
+
+
+
